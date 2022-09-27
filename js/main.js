deaf68611c9ae8deb85af418148075c0d2907c9d
levladik <vladislavtyshenko@yandex.ru>
2022-09-27 19:04:43 +0400

    add responsive menu

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+
+let body = document.querySelector('body');
+
+if (isMobile.any()) {
+	body.classList.add('touch');
+
+	let arrow = document.querySelectorAll('.nav-arrow');
+	let subMenu = document.querySelector('sub-nav__list');
+
+	arrow.addEventListener('click', function () {
+		subMenu.classList.toggle('open');
+		arrow.classList.toggle('active');
+	})
+} else {
+	body.classList.add('mouse');
+}
